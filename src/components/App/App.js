c510f658c30ed646f418807942465abf6b38752d
GMorse19 <malorse@comcast.net>
2020-07-03 10:25:57 -0400

    Added smileys to range input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import './App.css'
 
 import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
+import RangeSlider from '../RangeSlider/RangeSlider'
 
 function App() {
   return (
     <div className="App">
       <SignUp />
       <SignIn />
+      <RangeSlider />
     </div>
   );
 }
